050953c68432c3bb2056f9f5d8e012f537584357
Gustavo Alves da Silva <gustavoalves.softwaredev.2017@gmail.com>
2017-05-03 02:20:48 -0300

    First commit

---
+++ Livraria/src/Livro.java
@@ -0,0 +1,29 @@
+
+public class Livro {
+	String nome;
+	String descricao;
+	double valor;
+	String isbn;
+	Autor autor; // composição
+	
+	void mostrarDetalhes() {
+		String mensagem = "Mostrando detalhes do livro ";
+		System.out.println(mensagem);
+		System.out.println("Nome: " + nome);
+		System.out.println("Descrição: " + descricao);
+		System.out.println("Valor: " + valor);
+		System.out.println("ISBN: " + isbn);
+		if (this.temAutor()) {
+			autor.mostrarDetalhes();
+		}
+		System.out.println("--");
+	}
+	
+	public void aplicaDescontoDe(double porcentagem) {
+		this.valor -= this.valor * porcentagem;
+	}
+	
+	boolean temAutor() {
+		return this.autor != null;
+	}
+}
